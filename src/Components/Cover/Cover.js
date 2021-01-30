73cb79b016ae3912c6a619ee870553da90cfce61
Patrick Wehbe <pew02@mail.aub.edu>
2021-01-30 17:00:26 +0200

    filter

--- src/Components/Cover/Cover.js
+++ src/Components/Cover/Cover.js
@@ -1,8 +1,39 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import "./Cover.css";
 
 function Cover() {
-	return <div className="cover"></div>;
+	return (
+		<div className="cover">
+			<div className="cover__description">
+				<Grid container>
+					<Grid item xs={8}>
+						<Typography
+							variant="h2"
+							style={{ fontWeight: "bolder" }}>
+							IT Outsourcing
+						</Typography>
+						<Typography variant="h6">
+							We provide IT services to our customers as an
+							internal IT department in their organization
+						</Typography>
+						<Button
+							variant="contained"
+							style={{
+								backgroundColor: "#0071bb",
+								color: "white",
+								fontWeight: "bolder",
+							}}>
+							{" "}
+							Learn More
+						</Button>
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
 }
 
 export default Cover;
